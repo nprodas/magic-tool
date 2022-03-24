c44034c0cecb0defbfd624435fb5183affe497fb
Jean Roca <jeanroca@uark.edu>
2022-03-24 16:06:10 -0700

    broken

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# magic-tool
-A Discord bot using the Magic Eden API to provide you with real-time information for your favorite NFT project in a language you understand.
+# Magic Tool
+A Discord bot using the Magic Eden API to provide you with real-time information for your favorite NFT project in a language you understand. Looking to make the NFT space more accesible to people around the globe.
+
+# Capabilities
+Users have access to information such as:
+- Collection description
+- Current floor price
+- Active listings
+- Minimum portfolio value for a given wallet
+- Top NFTs determined by floor price for a given wallet
+- Localized roadmap for a collection
+
+All within Discord with simple commands and in over 100 different languages thanks to the Google Translate API
+
+# Supported Commands
+|Command | Description |
+|--------|-------------|
+|!collection <collection_name> | Displays collection statistics such as description, floor price, and active listings for specified collection. This will be sent in the channel specified language
+|!portfolio <wallet_address> | Displays the minimum portfolio value for a given wallet with a ranking of top 3 NFT's owned with highest floor price |
+|!set_language <language_code> | Sets the language for the specific channel. All future commands from Magic Tool in this channel will return information in the specified language. Default value is en |
+|!set_roadmap <roadmap>| Sets the roadmap for your server that can be translated depending on the language set for the channel this command is ran on |
+
+# Parameters
+| Parameter | Description |
+|-----------|-------------|
+|<collection_name> | Name that is found after the marketplace/ on a Magic Eden marketplace url |
+|<wallet_address> | Solana wallet Address |
+|<language_code> | iso639-1 language codes for [supported languages](https://py-googletrans.readthedocs.io/en/latest/) |
+| < roadmap > | Roadmap for your project collection |
+
+# Example Usage
+### ```!collection luckyllamalegion``` will show: 
+
+![collections](assets/collections.png)
+
+### ```!portfolio 6gjFy9Gp3mMN8uTLtfAyMmxdDfUe74YTo8cUTDXJtBUH``` will show:
+
+![portfolio](assets/portfolio.png)
+
+### ```!set_language hi``` will give you a confirmation and future commands in the text channel will show:
+
+![set_language](assets/set_language.png)
+
+### ```!set_roadmap <roadmap>``` followed by a ```!roadmap``` in a channel where ```!set_language es``` was ran will show:
+
+![roadmap](assets/roadmap.png)
--- metest.py
+++ metest.py
@@ -1,6 +1,9 @@
 import csv
-from operator import add
+from operator import add, itemgetter
 from os import getenv
+from turtle import title
+from webbrowser import get
+import discord
 from discord.ext import commands
 from googletrans import Translator
 import requests
@@ -38,17 +41,39 @@ def translate(msg, lang):
 
 def get_ME(url):
     payload={}
-    headers = {}
+    headers = {
+        "Key": key
+    }
     req = requests.request("GET", url, headers=headers, data=payload)
     ME_info = json.loads(req.text)
 
     return ME_info
 
+def get_Name(collectionName):
+    url = "http://api-mainnet.magiceden.dev/v2/collections/{}".format(collectionName)
+    payload={}
+    headers = {
+        "Key": key
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = json.loads(response.text)
+
+    return data['name']
+
+def construct_embed(place, name, collection_url, image_url):
+    embed = discord.Embed ( 
+        title = place + " " + name + " " + "- Magic Eden Link",
+        url = collection_url
+    )
+    embed.set_image(url = image_url)
+    return embed
+
 
 @bot.command()
 async def portfolio(ctx, wallet_address):
     wallet = wallet_address
-
+    nfts_owned = []
+    min_portfolio_value = 0
     # Get 500 NFT Tokens owned by wallet into a json
     url = "http://api-mainnet.magiceden.dev/v2/wallets/" + str(wallet) + "/tokens?offset=0&limit=500"
     payload={}
@@ -56,65 +81,53 @@ async def portfolio(ctx, wallet_address):
         "Key": key
     }
     response = requests.request("GET", url, headers=headers, data=payload)
-
     # Dump HTTP response into a json object
     data = json.loads(response.text)
 
-    # Initialize empty array and dictionaries we will use later
-    nfts_owned = []
-    images = {}
     floor_prices = {}
-    min_portfolio_value = 0
+    real_names = {}
+    collection_urls = {}
+    unique_collections = set()
+    for entry in data:
 
-    # Gather floor price for all nfts owned by wallet
-    for nft in data:
-        # NFTs that are not on Magic Eden are missing the collection field
-        # If the nft does not have a collection attribute it is not on ME so we skip
-        if not nft["collection"]:
+        # If the collection is not on ME
+        if "collection" not in entry:
             continue
 
-        nft_name = nft["name"]
-        collection_name = nft["collection"]
+        nft = {}
 
-        # Grab image link and add it to images dict
-        image_link = nft["image"]
-        images[collection_name] = image_link
+        nft["image_url"] = entry["image"]
+        nft["collection_name"] = entry["collection"]
+        collection_name = entry["collection"]
 
-        # Only add collection to floor_prices dictionary if it hasnt been added before
-        if collection_name not in floor_prices:
-            floor_prices[collection_name] = None
-        
-        # Get floor price for collections only if it has not been added before
-        if not floor_prices[collection_name]:
-            floor_prices[collection_name] = get_floor_price(collection_name)
-        
-        # Take care of duplicate NFT's from same collection
-        min_portfolio_value += floor_prices[collection_name]
-        # Add the specific nft into the nfts_owned array
-        nfts_owned.append(nft_name)
+        if collection_name not in unique_collections:
+            unique_collections.add(collection_name)
+            nft["floor_price"] = get_floor_price(entry["collection"])
+            floor_prices[collection_name] = nft["floor_price"]
 
-    print(floor_prices)
+            nft["real_name"] = get_Name(entry["collection"])
+            real_names[collection_name] = nft["real_name"]
 
-    top3 = sorted(floor_prices, key=floor_prices.get, reverse=True)[:3]
+            collection_info = get_ME("http://api-mainnet.magiceden.dev/v2/collections/{}".format(entry["collection"]))
+            collection_url = "http://magiceden.io/marketplace/{}".format(collection_info["symbol"])
+            nft["collection_url"] = collection_url
+            collection_urls[collection_name] = nft["collection_url"]
+        elif collection_name in unique_collections:
+            nft["floor_price"] = floor_prices[collection_name]
+            nft["real_name"] = real_names[collection_name]
+            nft["collection_url"] = collection_urls[collection_name]
 
-    image_links = []
-    for item in top3:
-        image_links.append(images[item])
-
-    # images[top3[collection_name]]
-
-    # Add up all floor prices
-    # min_portfolio_value = add_floor_prices(floor_prices)
-
-    await ctx.channel.send ("**Minimum Portfolio Value: **" + str(round(min_portfolio_value, 3)) + " SOL\n" +
-    "The top 3 Nfts are " +
-    image_links[0] + "\n" +
-    image_links[1] + "\n" +
-    image_links[2] 
-    )
-    # Call token mint for each token
-    # Gather Floor price for each collection
-    await ctx.channel.send(nfts_owned)
+        nfts_owned.append(nft)
+        min_portfolio_value += nft["floor_price"]
+    top3 = sorted(nfts_owned, key=itemgetter("floor_price"), reverse=True)[:3]
+    counter = 1
+    
+    await ctx.channel.send ("**Minimum Portfolio Value: **" + str(round(min_portfolio_value, 3)) + " SOL\n" + 
+    "**The top 3 NFTs for wallet " + str(wallet) + " are: **\n" + "\n" )
+    for nft in top3:
+        await ctx.channel.send(embed = construct_embed(":first_place:", nft["real_name"], nft["collection_url"], nft["image_url"]))
+        counter += 1
+    print(top3)
 
 @bot.command()
 async def collection(ctx, req, lang = ''):
